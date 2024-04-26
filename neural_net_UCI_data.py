7e307aef2fc2390aa0f1b083ea99579e49293e44
Bruno Pena-Lepe <bpena-lepe@cps.edu>
2024-04-26 11:28:41 -0500

    finisheduci

--- neural_net_UCI_data.py
+++ neural_net_UCI_data.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 from neural import *
+from sklearn.model_selection import train_test_split
 
 
 def parse_line(line: str) -> Tuple[List[float], List[float]]:
@@ -51,7 +52,9 @@ with open("wine_data.txt", "r") as f:
 td = normalize(training_data)
 
 nn = NeuralNet(13, 3, 1)
-nn.train(td, iters=100_000, print_interval=1000, learning_rate=0.1)
+train_d, test_d=train_test_split(td)
+nn.train(td, iters=10000, print_interval=1000, learning_rate=0.1)
 
 for i in nn.test_with_expected(td):
-    print(f"desired: {i[1]}, actual: {i[2]}")
+    difference = round(abs(i[1][0] - i[2][0]), 3)
+    print(f"desired: {i[1]}, actual: {i[2]} diff: {difference}")
